248ffb2989f139e4064445ccc216aaff2d16de85
Joe Ptacek <joe.ptacek@gmail.com>
2018-05-23 14:53:42 -0400

    testing

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+# Added at 2018-05-20 17:36:51 -0400 by joeptacek:
+gem "jekyll", "~> 3.8"
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// REQUIRE ---------------------------------------------------------------------
+
+// core
+var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+var bs = require('browser-sync').create();
+
+var child_jk_watch; // needs to be global so we can kill later
+
+var jk_build_command = [
+  'bundle',
+  'exec',
+  'jekyll',
+  'build'
+];
+
+var jk_watch_command = jk_build_command.concat('--watch');
+
+gulp.task('build', function () {
+  spawn(jk_build_command[0], jk_build_command.slice(1), { stdio: 'inherit' });
+});
+
+gulp.task('watch', function () {
+  spawn(jk_watch_command[0], jk_watch_command.slice(1), { stdio: 'inherit' });
+});
+
+gulp.task('serve', ['watch'], function () {
+  bs.init({
+    files: '_site/**',
+    server: '_site',
+    notify: false
+  });
+});
+
+gulp.task('default', ['serve']);
