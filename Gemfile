8f6f45547a32c2389428cbec276ad281fbcc3e0a
Joe Ptacek <joe.ptacek@gmail.com>
2018-11-16 13:08:18 -0500

    add js and lots of view toggle buttons, add octicons, no emoji for now

--- Gemfile
+++ Gemfile
@@ -8,3 +8,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Added at 2018-05-20 17:36:51 -0400 by joeptacek:
 gem "jekyll", "~> 3.8"
+
+group :jekyll_plugins do
+  gem "jekyll-octicons"
+end
---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+var VTContainers = document.getElementsByClassName('js-vt-container');
+if (VTContainers) {
+  function showHideInit(el) {
+    var elChildren = el.children;
+    for (var i = 0; i < elChildren.length; i++) {
+      elChildren[i].classList.add('dni');
+    }
+  }
+
+  function showMoreInit(el, showMoreInitVisible, showMoreResponsive) {
+    var elChildren = el.children;
+    for (var i = 0; i < elChildren.length; i++) {
+      if (i > showMoreInitVisible - 1) {
+        elChildren[i].classList.add('dni');
+        if (showMoreResponsive == "true") {
+          elChildren[i].classList.add('dbi-ns');
+        }
+      }
+    }
+  }
+
+  function showHide(el) {
+    var elChildren = el.children;
+    for (var i = 0; i < elChildren.length; i++) {
+      elChildren[i].classList.toggle('dni');
+    }
+  }
+
+  function showMore(el, showMoreMax) {
+    var flipCount = 0;
+    var elChildren = el.children;
+    var elVTContainer = el.closest('.js-vt-container');
+    var elVTButton = elVTContainer.querySelector('.js-vt-button');
+    for (var i = 0; i < elChildren.length; i++) {
+      if (elChildren[i].classList.contains('dni')) {
+        elChildren[i].classList.remove('dni');
+        elChildren[i].classList.remove('dni-ns');
+        flipCount++;
+        if (i < elChildren.length - 1) {
+          if (flipCount > showMoreMax - 1) {
+            break;
+          }
+        } else {
+          elVTButton.classList.add('dni');
+        }
+      }
+    }
+  }
+
+  function VTHandler(e) {
+    var clickedButtonEl = e.currentTarget;
+    var clickedVTContainer = clickedButtonEl.closest('.js-vt-container');
+    var clickedVTViewer = clickedVTContainer.querySelector('.js-vt-viewer');
+    var clickedVTViewerType = clickedVTViewer.getAttribute('data-vt-viewer-type');
+    var clickedVTViewerShowMoreMax = clickedVTViewer.getAttribute('data-vt-show-more-max');
+    switch (clickedVTViewerType) {
+      case 'show-hide':
+        showHide(clickedVTViewer);
+        clickedButtonEl.blur();
+        break;
+      case 'show-more':
+        showMore(clickedVTViewer, clickedVTViewerShowMoreMax);
+        clickedButtonEl.blur();
+        break;
+    }
+  }
+
+  for (var i = 0; i < VTContainers.length; i++) {
+    var thisVTContainer = VTContainers[i];
+    var thisVTButtonEl = thisVTContainer.querySelector('button');
+    var thisVTViewer = thisVTContainer.querySelector('.js-vt-viewer');
+    var thisVTViewerType = thisVTViewer.getAttribute('data-vt-viewer-type');
+    var thisVTViewerInitVisible = thisVTViewer.getAttribute('data-vt-show-more-init-visible');
+    var thisVTViewerShowMoreResponsive = thisVTViewer.getAttribute('data-vt-show-more-responsive');
+    switch (thisVTViewerType) {
+      case 'show-hide':
+        showHideInit(thisVTViewer);
+        break;
+      case 'show-more':
+        showMoreInit(thisVTViewer, thisVTViewerInitVisible, thisVTViewerShowMoreResponsive);
+        break;
+    }
+    thisVTButtonEl.addEventListener('click', VTHandler);
+  }
+}
